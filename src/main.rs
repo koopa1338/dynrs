06178ac94097f82f5a38fd8c554ed3935b1ecb0e
koopa1338 <sinner1991@gmail.com>
2020-12-21 01:47:11 +0100

    WIP: testing ureq for ip checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{thread, time};
+use ureq::Agent;
+
 fn main() {
-    println!("Hello, world!");
+    let agent = Agent::new();
+    let url = "http://checkip.spdns.de/";
+    loop {
+        let response = agent.get(url).call();
+        println!("{}", response.into_string().unwrap());
+        thread::sleep(time::Duration::new(5, 0));
+    }
 }
