523259aa0eb42f0d855c8c050508cfd51788b274
koopa1338 <sinner1991@gmail.com>
2021-01-03 16:00:58 +0100

    remove getters and use fields directly

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub struct DynConfig<'a> {
     protocol: &'a str,
     username: &'a str,
     token: &'a str,
-    delay: time::Duration,
+    pub delay: time::Duration,
 }
 
 impl DynConfig<'_> {
@@ -17,40 +17,20 @@ impl DynConfig<'_> {
         token: &'a str,
         delay: time::Duration,
     ) -> DynConfig<'a> {
-        return DynConfig {
+        DynConfig {
             url,
             protocol,
             username,
             token,
             delay,
-        };
-    }
-
-    pub fn get_url(&self) -> &str {
-        self.url
-    }
-
-    pub fn get_protocol(&self) -> &str {
-        self.protocol
-    }
-
-    pub fn get_username(&self) -> &str {
-        self.username
-    }
-
-    pub fn get_token(&self) -> &str {
-        self.token
-    }
-
-    pub fn get_delay(&self) -> time::Duration {
-        self.delay
+        }
     }
 }
 
 pub fn get_ip<'a>(agent: &Agent, config: &'a DynConfig) -> String {
-    match config.get_protocol() {
+    match config.protocol {
         "web" => {
-            let response = agent.get(config.get_url()).call();
+            let response = agent.get(config.url).call();
             return response.into_string().unwrap();
         }
         "if" => {
@@ -64,8 +44,8 @@ pub fn update_ip<'a>(agent: &Agent, config: &'a DynConfig, ip: &str) -> Request 
     // TODO: get the right url maybe an enum?
     let update_url = format!(
         "{}:{}@url/nic/update/{}",
-        config.get_username(),
-        config.get_token(),
+        config.username,
+        config.token,
         ip
     );
     return agent.post(&update_url);
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use ureq::Agent;
 fn main() {
     let agent = Agent::new();
     let url = "http://checkip.spdns.de/";
+    // TODO: use config file to set the fields
     let config: dynrs::DynConfig =
         dynrs::DynConfig::new(url, "web", "username", "token", time::Duration::new(5, 0));
     loop {
         let ip = dynrs::get_ip(&agent, &config);
         println!("### IP IS: {}", ip);
         dynrs::update_ip(&agent, &config, &ip);
-        thread::sleep(config.get_delay());
+        thread::sleep(config.delay);
     }
 }
