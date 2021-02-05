afd65559c66d198339e4c229331fcad49863240d
koopa1338 <sinner1991@gmail.com>
2021-02-05 05:51:45 +0100

    read settings from config file
    - support for json, hjson, toml, yaml, and ini files

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@ use dynrs::{DynConfig, Service, get_ip, update_ip, UPDATE_URL};
 use ureq::Agent;
 
 fn main() {
+    let mut settings = config::Config::default();
+    // TODO: specify config file as parameter otherwise fallback to default path.
+    settings.merge(config::File::with_name("config")).unwrap();
+
+    let checkip_url = match settings.get_str("check_url") {
+        Ok(url) => url,
+        Err(_) => UPDATE_URL.to_string(),
+    };
+    let username = match settings.get_str("username") {
+        Ok(username) => username,
+        Err(_) => panic!("No username found in config file"),
+    };
+
+    let token = match settings.get_str("token") {
+        Ok(token) => token,
+        Err(_) => panic!("No token found in config file"),
+    };
+
+    let ipv6 = match settings.get_bool("ipv6") {
+        Ok(ipv6) => ipv6,
+        Err(_) => false,
+    };
+
+    let config: DynConfig = DynConfig::new(&checkip_url, Service::Spdns{ipv6: ipv6}, &username, &token);
+
     let agent = Agent::new();
-    // TODO: use config file to set the fields
-    // if no check url is specified use the default
-    let checkip_url = UPDATE_URL;
-    let config: DynConfig = DynConfig::new(checkip_url, Service::Spdns{ipv6: false}, "username", "token");
     let ip = get_ip(&agent, &config).unwrap();
     println!("### IP IS: {}", ip);
     update_ip(&agent, &config, &ip);
